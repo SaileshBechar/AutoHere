c81ec3f406ea10a18dded3a2a7a15eed3e2c3860
Sailesh Bechar <sbechar@uwaterloo.ca>
2023-01-01 21:12:37 -0500

    Added proxy

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,10 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin/app")
 const twilio = require('twilio');
 const Polyline = require('@mapbox/polyline');
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
 admin.initializeApp();
 
 const accountSid = functions.config().twilio.sid;
@@ -54,3 +58,38 @@ exports.getDirections = functions.https.onRequest(async (req, res) => {
     duration: duration
   });
 })
+
+const basePlacesURL = "https://maps.googleapis.com/maps/api";
+const placesAPIKEY = functions.config().places.apikey;
+
+exports.getPlaces = functions.https.onRequest(async (req, res) => {
+  console.log("Autocomplete:", req.query)
+  let placesResp = await fetch(`${basePlacesURL}${requestWithKey}`)
+  res.status(200).send(placesResp)
+});
+
+
+app.use(createProxyMiddleware('/getPlaces', 
+  { 
+    target: `https://maps.googleapis.com/maps/api/`, 
+    changeOrigin: true,
+    pathRewrite: {
+      [`^/getPlaces`]: '',
+    },
+    onProxyReq(proxyReq, req, res) {
+      proxyReq.path += `&key=${placesAPIKEY}`
+      console.log("request:", req.query, proxyReq.path)
+    },
+    onError(err, req, res, target) {
+      res.writeHead(500, {
+        'Content-Type': 'text/plain',
+      });
+      console.log("Error:",err)
+      res.end('Something went wrong. And we are reporting a custom error message.');
+    },
+    onProxyRes(proxyRes, req, res) {
+      // console.log("Proxy Response", proxyRes)
+    } 
+  }));
+
+exports.api = functions.https.onRequest(app);
